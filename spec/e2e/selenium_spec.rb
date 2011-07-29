eef980619250940ffb2b699e37612f0156c585d5
Jari Bakken <jari.bakken@gmail.com>
2011-07-30 01:30:48 +0200

    Remove dead code.

--- spec/e2e/selenium_spec.rb
+++ spec/e2e/selenium_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe "Proxy + WebDriver" do
   let(:driver)  { Selenium::WebDriver.for :firefox, :profile => profile }
   let(:proxy) { new_proxy }
-  let(:listener) { BrowserMob::Proxy::WebDriverListener.new(proxy) }
-  let(:listening_driver)  { Selenium::WebDriver.for :firefox, :profile => profile, :listener => listener }
 
   let(:profile) {
     pr = Selenium::WebDriver::Firefox::Profile.new
@@ -22,7 +20,7 @@ describe "Proxy + WebDriver" do
     proxy.new_har("1")
     driver.get url_for("1.html")
 
-    proxy.new_page "2.com"
+    proxy.new_page "2"
     driver.get url_for("2.html")
 
     har = proxy.har
