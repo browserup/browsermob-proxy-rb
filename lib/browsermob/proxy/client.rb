a61433f4c9487cfadc971ea2783118b496f6b218
Kevin Menard <nirvdrum@gmail.com>
2013-01-18 21:32:21 -0500

    Allow specifying the proxy port to use.

--- lib/browsermob/proxy/client.rb
+++ lib/browsermob/proxy/client.rb
@@ -4,13 +4,15 @@ module BrowserMob
     class Client
       attr_reader :host, :port
 
-      def self.from(server_url)
+      def self.from(server_url, proxy_port = nil)
         # ActiveSupport may define Object#load, so we can't use MultiJson.respond_to? here.
         sm = MultiJson.singleton_methods.map { |e| e.to_sym }
         decode_method = sm.include?(:load) ? :load : :decode
 
+        payload = proxy_port.nil? ? '' : { :port => proxy_port.to_s }
+
         port = MultiJson.send(decode_method,
-          RestClient.post(URI.join(server_url, "proxy").to_s, '')
+          RestClient.post(URI.join(server_url, "proxy").to_s, payload)
         ).fetch('port')
 
         uri = URI.parse(File.join(server_url, "proxy", port.to_s))
